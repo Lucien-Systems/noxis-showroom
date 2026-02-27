cee2cb4cd3cd46e2e76c5d6c44ab36e02e600769
Adam Karl Lucien <68617017+AdamLucien@users.noreply.github.com>
2026-02-27 01:08:09 +0100

    fonts

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { jsx as _jsx } from "react/jsx-runtime";
-import "@fontsource/space-grotesk/400.css";
-import "@fontsource/space-grotesk/500.css";
-import "@fontsource/space-grotesk/600.css";
-import "@fontsource/space-grotesk/700.css";
-import "@fontsource/michroma/400.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/sora/500.css";
+import "@fontsource/sora/600.css";
+import "@fontsource/sora/700.css";
 import "./index.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import "@fontsource/space-grotesk/400.css";
-import "@fontsource/space-grotesk/500.css";
-import "@fontsource/space-grotesk/600.css";
-import "@fontsource/space-grotesk/700.css";
-import "@fontsource/michroma/400.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/sora/500.css";
+import "@fontsource/sora/600.css";
+import "@fontsource/sora/700.css";
 
 import "./index.css";
 
